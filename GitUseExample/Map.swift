8860b7041f7c35594a20117d7ca79cb53d9e2c79
Don Wilson <donwilson@dons-macbook-pro.local>
2015-08-08 09:26:05 -0700

    Add map feature initial files

---
+++ GitUseExample/Map.swift
@@ -0,0 +1,12 @@
+//
+//  Map.swift
+//  GitUseExample
+//
+//  Created by Don Wilson on 8/8/15.
+//  Copyright © 2015 Don Wilson. All rights reserved.
+//
+
+
+//  My new comment
+
+import Foundation
